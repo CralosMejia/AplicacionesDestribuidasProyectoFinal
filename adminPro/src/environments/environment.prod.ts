573e83ba11e874b25d86fec81e4b53168758c67f
Carlos Mejia <camh670@gmail.com>
2023-02-02 21:32:20 -0500

    Arreglango bugs

--- adminPro/src/environments/environment.prod.ts
+++ adminPro/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   base_url_login: 'http://apps24-001-site1.etempurl.com/api/Usuarios/Usuarios',
-  base_url_create_users:'https://microserviciocrearusuario.onrender.com',
-  base_url_get_users:'https://microservicioobtenerusuarios.onrender.com',
-  base_url_update_users:'https://microservicioactualizarusuario.onrender.com'
+  base_url_create_users:'https://microserviciocrearusuario.onrender.com/api/appsDistribuidas/createUser',
+  base_url_get_users:'https://microservicioobtenerusuarios.onrender.com/api/appsDistribuidas/getUsers',
+  base_url_update_users:'https://microservicioactualizarusuario.onrender.com/api/appsDistribuidas/updateUsers'
 };
--- getUsersMS/index.js
+++ getUsersMS/index.js
@@ -1,6 +1,6 @@
 const cors = require('cors');
 const express = require('express');
-const port = (process.env.port || 3000 );
+const port = (process.env.port || 3001 );
 
 //CONFIG SERVER
 //express
